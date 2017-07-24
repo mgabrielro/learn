a64c0abd46b7434738c1fe3c27ed4729334249d7
Gabriel Mandu <gabriel.mandu@check24.de>
2017-07-24 17:16:59 +0200

    add the declare js to the project

--- public/assets/desktop/dist/js/app.js
+++ public/assets/desktop/dist/js/app.js
@@ -359,6 +359,49 @@
 })(window, window.document, window.jQuery, undefined);
 
 /**
+ * @namespace c24
+ * @name c24.declare
+ * @author Andreas Frömer <andreas.froemer@check24.de>
+ */
+(function(window) {
+    "use strict";
+
+    var c24 = window.c24 = window.c24 || {};
+
+    /**
+     * Declare a new object from given fqn
+     *
+     * @param {String} name
+     * @param {Function|Array} deps
+     * @param {Function} factory
+     */
+    function declare(name, deps, factory) {
+        var parts = name.split('.'),
+            parent = window,
+            currentPart = '';
+
+        factory = factory || (typeof deps === 'function' ? deps : function() {});
+        deps = deps.hasOwnProperty('length') ? deps : [];
+
+        for(var i = 0, length = parts.length-1; i < length; i++) {
+            currentPart = parts[i];
+            parent[currentPart] = parent[currentPart] || {};
+            parent = parent[currentPart];
+        }
+
+        var lastPart = parts[parts.length-1];
+
+        if (!!parent[lastPart]) {
+            throw 'Cannot redeclare: ' +  name;
+        }
+
+        parent[lastPart] = factory.apply(window, deps);
+    }
+
+    c24.declare = declare;
+
+})(window);
+/**
  * Gets and/or creates a namespace object by its full qualified name.
  * Also runs a given callback method with the scope of the passed in namespace.
  *
